28570448f6a971bf14072f9ee669e19669182295
PiyushJT <piyushthummar7@gmail.com>
2025-11-04 09:43:08 +0530

    Added some programs

---
+++ 5. Mutable Data Structures/Program434.py
@@ -0,0 +1,8 @@
+"""
+
+Write a Python program to calculate the sum of the positive and negative numbers of a given list of numbers using
+lambda function.
+
+"""
+
+numbers = [10, -5, 3, -1, 7, -2, 8, -4]
