13862da9a58c95314a513e60f1b2ce3fb2ee85d1
PiyushJT <piyushthummar7@gmail.com>
2025-11-06 09:50:32 +0530

    Added two more programs

--- 5. Mutable Data Structures/Program440.py
+++ 5. Mutable Data Structures/Program440.py
@@ -24,12 +24,27 @@ and an integer determining whether to break things up by threes,
 fours, or whatever user inputs.
 Encrypt the string using above method.
 
+⚠️ Output of (B) is wrong in example
+
 (B). If you get a message which is encoded by the method above then,
 Write a decryption program for the general case.
 Taking input of any encrypted string from user
 with key number used while breaking message apart during encryption.
 
+(C). From the output string (Output Decrypted Message) of above
+program (Part-B), create a Dictionary with Key as First
+Character and Value as list of words Starting with that Character
+from above string.
+And print that dictionary by sorting it
+based on the number of elements in a list of values in descending order.
+
+Note: Consider capital and lower first character of words as
+same character in this program. For ex. ‘Hi’ and ‘hello’ both
+will be considered starting from ‘h’.
+
 """
+
+import math
 
 def encode(str, gap):
     
@@ -40,18 +55,73 @@ def encode(str, gap):
     return encoded
 
 
-# ⚠️ Baaki
+
 def decode(str, gap):
-    return str
+
+    numCols = math.ceil(len(str) / gap)
+    numRows = gap
+    numShadedBoxes = (numCols * numRows) - len(str)
+    plainText = [""] * numCols
+    col = 0
+    row = 0
+
+    for symbol in str:
+        plainText[col] += symbol
+        col += 1
+
+        if (
+            (col == numCols)
+            or (col == numCols - 1)
+            and (row >= numRows - numShadedBoxes)
+        ):
+            col = 0
+            row += 1
+
+    return "".join(plainText)
 
 
-ch = input("1. Encrypt\n2. Decrypt\nEnter your choice: ")
+
+def getDict(str):
+
+    list = str.lower().split()
+    dict = {}
+ 
+    for word in list:
+        key = word[0]
+ 
+        if key not in dict.keys():
+            dict[key] = []
+        
+        dict[key].append(word)
+
+    sorted_list = sorted(dict.items(), key = lambda x: len(x[1]), reverse = True)
+
+    dict = {}
+
+    for i in sorted_list:
+        key = i[0]
+        value = i[1]
+        dict[key] = value
+
+    return dict
+
+
+
+
+ch = input("1. Encrypt\n2. Decrypt\n3. Get Dictionary\nEnter your choice: ")
 
 str = input("Enter the string: ")
-gap = int(input("Enter the gap: "))
 
 if ch == '1':
+    gap = int(input("Enter the gap: "))
     print(encode(str, gap))
 
 elif ch == '2':
+    gap = int(input("Enter the gap: "))
     print(decode(str, gap))
+
+elif ch == '3':
+    print(getDict(str))
+
+else:
+    print("Invalid choice.")
